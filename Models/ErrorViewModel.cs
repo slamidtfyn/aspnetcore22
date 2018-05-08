7c3f7baf42dde4e780734ce57fec7bdb521c8c8f
Søren Larsen <sl@komit.nu>
2018-05-08 10:25:26 +0200

    dnc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mvc080518.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
